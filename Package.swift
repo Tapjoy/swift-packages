e52096e9ae38acda5b5f8709107072c3d4a1671e
Yamato CI <tapjoy_sdk@unity3d.com>
2025-06-03 11:02:47 +0000

    Automated Update to 14.4.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-.binaryTarget(name: "Tapjoy", url: "https://sdk.tapjoy.com/releases/14.3.1/SwiftPackage/Tapjoy.xcframework.zip", checksum: "16b23673d8da71b4e2de03de7328dc24a32c17896ae76a9d2961662711e4b6e9"),
+.binaryTarget(name: "Tapjoy", url: "https://sdk.tapjoy.com/releases/14.4.0/SwiftPackage/Tapjoy.xcframework.zip", checksum: "57a8766d43702787f96d39da636f9f68e1c522d573bd1f20e698522eeacabee8"),
     ]
 )
 
